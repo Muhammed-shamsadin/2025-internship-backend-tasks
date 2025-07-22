c629d6830e900561cc8196b90b2e1d3bed4c9ac9
muhammed-shamsadin <muhammedsamsoon@gmail.com>
2025-07-22 14:56:13 +0300

    fix: case insensitive and ignored spaces, punctuations

--- Task-2/Palindrome-Check/main.go
+++ Task-2/Palindrome-Check/main.go
@@ -2,23 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func palindrome_check(str string) string {
+	var cleaned []rune
+	for _, r := range str {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') {
+			cleaned = append(cleaned, rune(strings.ToLower(string(r))[0]))
+		}
+	}
 	left := 0
-	right := len(str) - 1
-
+	right := len(cleaned) - 1
 	for left < right {
-		if str[left] != str[right] {
+		if cleaned[left] != cleaned[right] {
 			return "Not a palindrome"
 		}
 		left++
 		right--
 	}
-
 	return "Palindrome"
 }
-
 
 func main() {
 	var str string
@@ -27,4 +31,3 @@ func main() {
 	fmt.Println(result)
 
 }
-
